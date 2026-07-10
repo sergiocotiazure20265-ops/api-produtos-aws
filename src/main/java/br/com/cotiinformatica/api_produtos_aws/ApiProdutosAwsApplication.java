fe5e78b0cf76476a4df7c7883b7af6b925a5aca7
sergiocotiazure20265-ops <sergiocotiazure20265@gmail.com>
2026-07-10 19:41:09 -0300

    versão inicial

---
+++ src/main/java/br/com/cotiinformatica/api_produtos_aws/ApiProdutosAwsApplication.java
@@ -0,0 +1,13 @@
+package br.com.cotiinformatica.api_produtos_aws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiProdutosAwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiProdutosAwsApplication.class, args);
+	}
+
+}
